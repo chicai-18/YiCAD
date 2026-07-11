9797a2659436acd28435affff09f19278c34e05f
chicai-18 <chicai_123@163.com>
2026-07-11 21:28:24 +0800

    add precompiled header

---
+++ YiCAD/src/main/YiCadPch.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <algorithm>
+#include <cmath>
+#include <cstdint>
+#include <memory>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include <QAction>
+#include <QMouseEvent>
+#include <QObject>
+#include <QString>
+#include <QWidget>
